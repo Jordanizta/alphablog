fbd5b0307ed4fa0af4838331970893662df47d06
Emanuel Jordan da Silva Ribeiro <p0600583@D19600L.sefin.fortaleza.ce.gov.br>
2019-11-21 11:56:16 -0300

    Poucas mudanças em views e  formulário de inserção

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
-    validates :title, :description, :autor, presence: true
+    validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+    validates :autor, presence: true, length: { minimum: 3, maximum: 40 }
 
 end
