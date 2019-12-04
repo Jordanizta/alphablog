f4721a6c3bbfdce6b1e6d1abd4431e8012687d35
Emanuel Jordan da Silva Ribeiro <p0600583@D19600L.sefin.fortaleza.ce.gov.br>
2019-12-04 12:03:17 -0300

    Criado formulário de login e Criação e Destruição de sessões de login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+    def new
+        
+    end
+
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id 
+            flash[:sucess] = "Login realizado com sucesso"
+            redirect_to user_path(user)
+
+        else
+            flash.now[:danger] = "Algo de errado com suas informações de login. Por favor, tente novamente."
+            render 'new'
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:sucess] = "Desconectado com sucesso"
+        redirect_to root_path
+    end
+    
+end
