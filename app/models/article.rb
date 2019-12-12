2f597819012f84c47f3c92217fd56ddfef4240f8
root <root@D19600L.sefin.fortaleza.ce.gov.br>
2019-12-12 12:13:32 -0300

    ArticleCategory db

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
     belongs_to :user
+    has_many :article_categories #muitas categorias de artigos
+    #muitas categorias pelas categorias de artigos
+    has_many :categories, through: :article_categories
     validates :title, presence: true, length: { minimum: 3, maximum: 50 }
     validates :description, presence: true, length: { minimum: 10, maximum: 300 }
     validates :autor, presence: true, length: { minimum: 3, maximum: 40 }
