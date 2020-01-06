17fa49a79b460fcd70c614242f224dcf362c0899
Emanuel Jordan da Silva Ribeiro <e06124643@D19600L.sefin.fortaleza.ce.gov.br>
2020-01-06 12:03:41 -0300

    Faz relação de User/Article e Reaction

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,7 @@ class Article < ApplicationRecord
     has_many :article_categories #muitas categorias de artigos
     #muitas categorias pelas categorias de artigos
     has_many :categories, through: :article_categories
+    has_many :reactions, dependent: :destroy
     validates :title, presence: true, length: { minimum: 3, maximum: 50 }
     validates :description, presence: true, length: { minimum: 10, maximum: 300 }
     validates :autor, presence: true, length: { minimum: 3, maximum: 40 }
