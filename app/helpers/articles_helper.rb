7332cac5ae77aab50a36d537787bc033a84be599
Emanuel Jordan da Silva Ribeiro <e06124643@D19600L.sefin.fortaleza.ce.gov.br>
2020-02-11 11:58:53 -0300

    Método helper para botões de ação em artigos

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,10 @@
 module ArticlesHelper
+  def adm_article_buttons(article, user)
+    if logged_in? && current_user == user
+        concat link_to("Editar esse artigo", edit_article_path(article), class: "btn btn-xs btn-primary")
+        concat link_to("Deletar esse artigo", article_path(article), method: :delete, class: "btn btn-xs btn-danger",        
+                data: {confirm: "Certeza que deseja deletar?"})
+    end
+  end
+
 end
